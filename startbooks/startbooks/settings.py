dd9e8039a8b0c759543b7cbebc5aecb121ee7124
BrunaCardoso7 <obitadrawing@gmail.com>
2025-02-28 12:56:26 -0300

    chore: atualiza o banco para uma instância postgresql

--- startbooks/startbooks/settings.py
+++ startbooks/startbooks/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,13 +134,15 @@ CHANNEL_LAYERS = {
     }
 }
 
+load_dotenv()
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'startbooks',
-        'USER': 'brunadev',  
-        'PASSWORD': 'bruna8596',  
-        'HOST': 'startbooks_bd',  
-        'PORT': '5432',  
+        'NAME': 'startbooks_db',
+        'USER': 'brunadev',
+        'PASSWORD': 'Mghc1kQpmfHevlFpr7Aj9yFX4BueH0qa',
+        'HOST': 'dpg-cv0tmebtq21c73es78dg-a.oregon-postgres.render.com',
+        'PORT': '5432',
     }
 }
